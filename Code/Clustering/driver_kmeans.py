476a5f54044fb05f09ccf9f8d2833562d6e5f3e2
kristokrugger-purefda <kristo@purefda.com>
2020-12-17 14:07:49 -0800

    added code for silhouette and calculation in driver_kmeans

--- Code/Clustering/driver_kmeans.py
+++ Code/Clustering/driver_kmeans.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import plot_data
 import time
+import metrics
 
 # (1) generate data
 nsample = 300
@@ -23,6 +24,10 @@ start = time.time()
 list_objective = model.fit(X,nepoch)
 end = time.time()
 print("Training time (Kmeans): ".format(end - start))
+cluster_labels = model.clustersave[-1]
+dist_func = lambda x1, x2: np.sqrt(np.dot(x1-x2,x1-x2))
+sil_score = metrics.silhouette_score(X, cluster_labels, dist_func)
+print(f"Silhouette Score: {sil_score}")
 # (4) plot results
 plot_data.plot_objective(list(range(len(list_objective))),list_objective,
 	title="K Means Clustering",xlabel="Iteration",ylabel="Objective")
---
+++ Code/Clustering/metrics.py
@@ -0,0 +1,29 @@
+# metrics.py
+
+import numpy as np
+
+def silhouette(i, X, cluster_labels, dist_func):
+    """Calculate the silhouette value for a given point X_i."""
+    # calculate avg distance between X_i and every other point in its cluster
+    cluster_label = cluster_labels[i]
+    cluster_i = np.squeeze(np.argwhere(cluster_labels == cluster_label))
+    a_i = np.sum([dist_func(X[:,i], X[:,j]) for j in cluster_i]) / (len(cluster_i)-1)
+    # calculate avg distance between X_i and every other point in its closest cluster
+    min_dist = float('inf')
+    k = None
+    for l in range(X.shape[1]):
+        if cluster_labels[l] != cluster_label:
+            dist = dist_func(X[:,i], X[:,l])
+            if dist < min_dist or k is None:
+                min_dist = dist
+                k = cluster_labels[l]
+    cluster_k = np.squeeze(np.argwhere(cluster_labels == k))
+    b_i = np.mean([dist_func(X[:,i], X[:,k]) for k in cluster_k])
+    # calculate silhouette value for X_i
+    s_i = (b_i - a_i) / max(b_i, a_i)
+    return s_i
+
+def silhouette_score(X, cluster_labels, dist_func):
+    """Calculate the average silhouette value of all points."""
+    S = np.mean([silhouette(i, X, cluster_labels, dist_func) for i in range(X.shape[1])])
+    return S
