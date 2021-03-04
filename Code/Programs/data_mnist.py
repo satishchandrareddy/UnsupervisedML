59f58f735dc1fe4b9ba2726df20312976bdd6c6c
satishchandrareddy <63747108+satishchandrareddy@users.noreply.github.com>
2021-03-04 10:04:06 -0800

    2021.03.04 - Update
    data_mnist.py

--- Code/Programs/data_mnist.py
+++ Code/Programs/data_mnist.py
@@ -9,6 +9,23 @@ class mnist:
     def __init__(self):
         self.root_dir = Path(__file__).resolve().parent.parent
 
+    def load_valid(self,nsample):
+    	# load mnist valid dataset
+    	# create feature matrix with nsample data points 
+	    # read data test
+	    df = pd.read_csv(self.root_dir / "Data_MNIST/MNIST_valid_10K.csv")
+	    # get labels and 
+	    class_label = df["label"]
+	    # remove label column
+	    df = df.drop(columns="label")
+	    # create feature matrix from remaining data - divide by 255 and take transpose
+	    X = df.values.T/255
+	    # get requested number of data samples
+	    X = X[:,:nsample]
+	    class_label = class_label[:nsample]
+	    print("X.shape: {} - class_label.shape: {}".format(X.shape,class_label.shape))
+	    return X,class_label
+
     def load_train(self,nsample):
     	# load mnist data
     	# create feature matrix with nsample data points 
@@ -16,9 +33,9 @@ class mnist:
 	    df1 = pd.read_csv(self.root_dir / "Data_MNIST/MNIST_train_set1_30K.csv")
 	    df2 = pd.read_csv(self.root_dir / "Data_MNIST/MNIST_train_set2_30K.csv")
 	    # get labels and concatenate
-	    Y1 = df1["label"].values
-	    Y2 = df2["label"].values
-	    Y = np.concatenate((Y1,Y2))
+	    class_label1 = df1["label"].values
+	    class_label2 = df2["label"].values
+	    class_label = np.concatenate((class_label1,class_label2))
 	    # remove label column
 	    df1 = df1.drop(columns="label")
 	    df2 = df2.drop(columns="label")
@@ -27,36 +44,18 @@ class mnist:
 	    X1 = df1.values/255
 	    X2 = df2.values/255
 	    X = np.concatenate((X1,X2),axis=0).T
-	    # get requested number of training data samples
+	    # get requested number of data samples
 	    X = X[:,:nsample]
-	    Y = Y[:nsample]
-	    print("X.shape: {} - Y.shape: {}".format(X.shape,Y.shape))
-	    return X,Y
-
-    def load_valid(self,nsample):
-    	# load mnist data
-    	# create feature matrix with nsample data points 
-	    # read data test
-	    df = pd.read_csv(self.root_dir / "Data_MNIST/MNIST_valid_10K.csv")
-	    # get labels and 
-	    Y = df["label"]
-	    # remove label column
-	    df = df.drop(columns="label")
-	    # create feature matrix from remaining data - divide by 255
-	    # concatenate and take transpose
-	    X = df.values.T/255
-	    # get requested number of training data samples
-	    X = X[:,:nsample]
-	    Y = Y[:nsample]
-	    print("X.shape: {} - Y.shape: {}".format(X.shape,Y.shape))
-	    return X,Y
+	    class_label = class_label[:nsample]
+	    print("X.shape: {} - class_label.shape: {}".format(X.shape,class_label.shape))
+	    return X,class_label
 
     def plot_image(self,X,seed,array_idx):
         # create 5x5 subplot of mnist images
         # X is feature matrix
         # seed is integer used to set up random seed
         # array_idx is numpy array of least length 25
-        # randomly choose 25 indices from array_idx -> these are images to be plotted
+        # choose 25 indices at random from array_idx -> these are images to be plotted
         np.random.seed(seed)
         array_idx_plot = np.random.choice(array_idx,25,replace=False)
         nrow = 5
@@ -68,6 +67,7 @@ class mnist:
         fig.suptitle("Images of Sample MNIST Digits")
         for row in range(nrow):
             for col in range(ncol):
+            	# convert feature vector (length 784) into 28x28 2darray before plotting
                 digit_image = np.flipud(np.reshape(X[:,array_idx_plot[idx]],(npixel_width,npixel_height)))
                 ax[row,col].set_aspect("equal")
                 ax[row,col].pcolormesh(digit_image,cmap="Greys")
@@ -78,7 +78,7 @@ if __name__ == "__main__":
 	mnist_object = mnist()
 	# load data
 	nsample = 60000
-	X, _ = mnist_object.load(nsample)
+	X,_ = mnist_object.load_train(nsample)
 	# plot 25 random images from dataset
 	seed = 11
 	array_idx = np.arange(nsample)
