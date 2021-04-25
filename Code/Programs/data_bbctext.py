9f6cf34bf4a2f34e215952e0f0bd8e294c1dc115
satishchandrareddy <63747108+satishchandrareddy@users.noreply.github.com>
2021-04-24 17:02:25 -0700

    2021.04.24 Update
    Update

--- Code/Programs/data_bbctext.py
+++ Code/Programs/data_bbctext.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import CountVectorizer
 from pathlib import Path
 from wordcloud import WordCloud
 
